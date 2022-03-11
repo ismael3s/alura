d9a32d36c36ede89453c74c65e3de4e36a62ffff
Ismael Santana <souz4ismael@gmail.com>
2022-03-11 00:41:41 -0300

    finished first course

--- go/01/main.go
+++ go/01/main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
+const monitiringInterval = 2
+const delay = 1
+
 func main() {
-	showMenu()
+	for {
+		showMenu()
 
-	input := readCommand()
+		input := readCommand()
 
-	switch input {
-	case 1:
-		startMonitoring()
-	case 2:
-		fmt.Println("Showing logs")
-	case 0:
-		fmt.Println("Exiting...")
-	default:
-		fmt.Println("Invalid input")
+		switch input {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Showing logs")
+			showLogs()
+		case 0:
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid input")
+			os.Exit(-1)
+		}
 	}
 
 }
@@ -40,13 +54,80 @@ func readCommand() int {
 func startMonitoring() {
 	fmt.Println("Monitoring...")
 
-	website := "https://www.google.com"
+	websites := readFromTxt()
 
-	response, err := http.Get(website)
+	for i := 0; i < monitiringInterval; i++ {
+		for _, website := range websites {
+			isWebsiteUp(website)
+		}
+
+		time.Sleep(time.Second * delay)
+	}
+	fmt.Println("")
+
+}
+
+func isWebsiteUp(url string) {
+	response, _ := http.Get(url)
+
+	if response.StatusCode == 200 {
+		fmt.Println("Website", url, "is up!")
+		registerLog(url, true)
+	} else {
+		registerLog(url, false)
+		fmt.Println("Website", url, "is down!")
+	}
+}
+
+func readFromTxt() []string {
+	file, err := os.Open("sites.txt")
 
 	if err != nil {
-		log.Fatal("Website is down")
+		log.Fatalln("Failed to open websites files", err)
 	}
 
-	fmt.Println(response.Status)
+	defer file.Close()
+
+	var sites []string
+
+	reader := bufio.NewReader(file)
+
+	for {
+		r, err := reader.ReadString('\n')
+
+		row := strings.TrimSpace(r)
+
+		sites = append(sites, row)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	return sites
+}
+
+func registerLog(site string, isUp bool) {
+	file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println("Failed to register the log", err)
+	}
+
+	defer file.Close()
+
+	message := fmt.Sprintf("%s - %s isUp %t.\n", time.Now().Local().Format("2006-01-02 15:04:05"), site, isUp)
+
+	file.WriteString(message)
+}
+
+func showLogs() {
+	file, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		log.Fatalln("Failed to open log file", err)
+	}
+
+	fmt.Println(string(file))
 }
